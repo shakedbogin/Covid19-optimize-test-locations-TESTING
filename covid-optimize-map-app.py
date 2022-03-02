4a47aff06b0103199d28699ab8b99630114c256c
Shaked Bogin <shakedbogins@gmail.com>
2022-03-02 13:35:59 +0200

    Update covid-optimize-map-app.py

--- covid-optimize-map-app.py
+++ covid-optimize-map-app.py
@@ -1,1 +1,32 @@
+from pulp import *
+import pandas as pd
+import numpy as np
+import str 
+import re
+from geopy.geocoders import Nominatim
+import folium
+import math
+import pickle
+from IPython.display import Image
+import matplotlib.pyplot as plt
+import datetime 
+import timeit
+import streamlit as st
+
+
+df = pd.read_excel(r'distance-matrix-TEST.xlsx',index_col=0)
+df =  df.reset_index(drop=True)
+
+
+st.write('# bla')
+st.markdown('#')  
+st.write('### bla')
+st.write('bla')
+st.markdown('#')  
+
+
+ st.markdown('#')  
+ st.write('### View samples of the data you uploaded')
+ st.dataframe(df.head(),3000,500)
+
 
