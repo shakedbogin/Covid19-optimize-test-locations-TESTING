f3d2bc38f121852679fe985d632177af8ec156e3
Shaked Bogin <shakedbogins@gmail.com>
2022-03-02 16:39:53 +0200

    Update covid-optimize-map-app.py

--- covid-optimize-map-app.py
+++ covid-optimize-map-app.py
@@ -14,15 +14,8 @@ import datetime
 import timeit
 import streamlit as st
 
-import requests
 
-url= 'https://github.com/shakedbogin/covid-optimize-map/blob/83c28e5f5a5bc3a7b01771451a160a376680e64b/distance-matrix-TEST.xlsx'
-myfile = requests.get(url)
-# myfile = xlsx.File(requests.get(url).body)
-
-df=pd.read_excel(myfile.content,index_col=0)
-
-# df = pd.read_excel('./distance-matrix-TEST.xlsx',index_col=0)
+df = pd.read_csv('./distance-matrix-TEST.csv',encoding='utf-8')
 # df =  df.reset_index(drop=True)
 
 st.write('# bla')
